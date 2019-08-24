acaba6140e970050c689e60afc6fe6bd253977fe
Jayesh <jayesh.patel.2197@gmail.com>
2019-08-24 10:26:15 +0530

    Initial Commit

---
+++ src/main/java/com/finalassessment/ubinge/UBingeApplication.java
@@ -0,0 +1,13 @@
+package com.finalassessment.ubinge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UBingeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UBingeApplication.class, args);
+    }
+
+}
