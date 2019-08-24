8863eae6c53bb039feca7c2c002c9e51a6fc28f6
Jayesh <jayesh.patel.2197@gmail.com>
2019-08-24 15:04:26 +0530

    Add Delivery Guy Controller.

---
+++ src/main/java/com/finalassessment/ubinge/controller/DeliveryGuyController.java
@@ -0,0 +1,48 @@
+package com.finalassessment.ubinge.controller;
+
+import com.finalassessment.ubinge.model.DeliveryGuy;
+import com.finalassessment.ubinge.service.DeliveryGuyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class DeliveryGuyController {
+    private DeliveryGuyService deliveryGuyService;
+
+    @Autowired
+    public DeliveryGuyController(DeliveryGuyService deliveryGuyService) {
+        this.deliveryGuyService = deliveryGuyService;
+    }
+
+    @GetMapping(value = "/deliveryguys")
+    public List<DeliveryGuy> getAllDeliveryGuys() {
+        return deliveryGuyService.findAll();
+    }
+
+    @GetMapping(value = "/deliveryguys/{deliveryGuyId}")
+    public DeliveryGuy getDeliveryGuy(@PathVariable Long deliveryGuyId) {
+        return deliveryGuyService.findById(deliveryGuyId);
+    }
+
+    @PostMapping(value = "/deliveryguys")
+    public DeliveryGuy saveDeliveryGuy(@RequestBody DeliveryGuy deliveryGuyId) {
+        return  deliveryGuyService.save(deliveryGuyId);
+    }
+
+    @PutMapping(value = "/deliveryguys/{deliveryGuyId}")
+    public DeliveryGuy updateDeliveryGuy(@RequestBody DeliveryGuy deliveryGuy, @PathVariable Long deliveryGuyId) {
+        return deliveryGuyService.update(deliveryGuy, deliveryGuyId);
+    }
+
+    @DeleteMapping(value = "/deliveryguys")
+    public void deleteDeliveryGuy(@RequestBody DeliveryGuy deliveryGuy) {
+        deliveryGuyService.delete(deliveryGuy);
+    }
+
+    @DeleteMapping(value = "/deliveryGuys/{deliveryGuyId}")
+    public void deleteDeliveryGuyById(@PathVariable Long deliveryGuyId) {
+        deliveryGuyService.deleteById(deliveryGuyId);
+    }
+}
