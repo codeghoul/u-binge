e0d224eb4263c4c5cb772a2f5b62495cac4fd247
Jayesh <jayesh.patel.2197@gmail.com>
2019-08-25 23:31:49 +0530

    Add accidentally deleted implementations from Customer Controller.

--- src/main/java/com/finalassessment/ubinge/controller/CustomerController.java
+++ src/main/java/com/finalassessment/ubinge/controller/CustomerController.java
@@ -1,9 +1,13 @@
 package com.finalassessment.ubinge.controller;
 
+import com.finalassessment.ubinge.model.Customer;
 import com.finalassessment.ubinge.service.CustomerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -15,5 +19,39 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    
+    @GetMapping(value = "/customers")
+    public List<Customer> getAllCustomers() {
+        log.debug("Getting All Customers.");
+        return customerService.findAll();
+    }
+
+    @GetMapping(value = "/customers/{customer_id}")
+    public Customer getCustomer(@PathVariable Long customer_id) {
+        log.debug("Getting Customers By Id.");
+        return customerService.findById(customer_id);
+    }
+
+    @PostMapping(value = "/customers")
+    public Customer saveCustomer(@RequestBody Customer customer) {
+        log.debug("Saving Customer.");
+        return  customerService.save(customer);
+    }
+
+    @PutMapping(value = "/customers/{customer_id}")
+    public Customer updateCustomer(@RequestBody Customer customer, @PathVariable Long customer_id) {
+        log.debug("Updating Customer.");
+        return customerService.update(customer, customer_id);
+    }
+
+    @DeleteMapping(value = "/customers")
+    public void deleteCustomer(@RequestBody Customer customer) {
+        log.debug("Deleting Customer.");
+        customerService.delete(customer);
+    }
+
+    @DeleteMapping(value = "/customers/{customer_id}")
+    public void deleteCustomerById(@PathVariable Long customer_id) {
+        log.debug("Deleting Customer By Id.");
+        customerService.deleteById(customer_id);
+    }
 }
