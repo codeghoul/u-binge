67d574b1b38f9d895766e38eb478828b4431ef7f
Jayesh <jayesh.patel.2197@gmail.com>
2019-08-26 11:58:14 +0530

    Modify DeliveryGuyController to return ResponseEntities.

--- src/main/java/com/finalassessment/ubinge/controller/DeliveryGuyController.java
+++ src/main/java/com/finalassessment/ubinge/controller/DeliveryGuyController.java
@@ -6,6 +6,8 @@ import com.finalassessment.ubinge.service.DeliveryGuyService;
 import com.finalassessment.ubinge.vo.OrderModificationVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,15 +23,27 @@ public class DeliveryGuyController {
     }
 
     @GetMapping(value = "/deliveryguys")
-    public List<DeliveryGuy> getAllDeliveryGuys() {
+    public ResponseEntity<List<DeliveryGuy>> getAllDeliveryGuys() {
         log.debug("Getting all Delivery Guys.");
-        return deliveryGuyService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(deliveryGuyService.findAll());
     }
 
     @GetMapping(value = "/deliveryguys/{deliveryGuyId}")
-    public DeliveryGuy getDeliveryGuy(@PathVariable Long deliveryGuyId) {
+    public ResponseEntity<DeliveryGuy> getDeliveryGuy(@PathVariable Long deliveryGuyId) {
         log.debug("Getting Delivery Guy by id.");
-        return deliveryGuyService.findById(deliveryGuyId);
+        return ResponseEntity.status(HttpStatus.OK).body(deliveryGuyService.findById(deliveryGuyId));
+    }
+
+    @GetMapping(value = "/deliveryguys/{deliveryGuyId}/orders")
+    public ResponseEntity<List<Order>> getDeliveryGuyOrders(@PathVariable Long deliveryGuyId) {
+        log.debug("Getting Orders assigned to Delivery Guy.");
+        return ResponseEntity.status(HttpStatus.OK).body(deliveryGuyService.getDeliveryGuyOrders(deliveryGuyId));
+    }
+
+    @GetMapping(value = "/deliveryguys/{deliveryGuyId}/orders/{orderId}")
+    public ResponseEntity<Order> getDeliveryGuyOrderById(@PathVariable Long deliveryGuyId, @PathVariable Long orderId) {
+        log.debug("Getting Order assigned to Delivery Guy using given OrderId.");
+        return ResponseEntity.status(HttpStatus.OK).body(deliveryGuyService.getDeliveryGuyOrderById(deliveryGuyId, orderId));
     }
 
     @PostMapping(value = "/deliveryguys")
@@ -39,36 +53,28 @@ public class DeliveryGuyController {
     }
 
     @PutMapping(value = "/deliveryguys/{deliveryGuyId}")
-    public DeliveryGuy updateDeliveryGuy(@RequestBody DeliveryGuy deliveryGuy, @PathVariable Long deliveryGuyId) {
+    public ResponseEntity<DeliveryGuy> updateDeliveryGuy(@RequestBody DeliveryGuy deliveryGuy, @PathVariable Long deliveryGuyId) {
         log.debug("Updating Delivery Guy by id.");
-        return deliveryGuyService.update(deliveryGuy, deliveryGuyId);
-    }
-
-    @DeleteMapping(value = "/deliveryguys")
-    public void deleteDeliveryGuy(@RequestBody DeliveryGuy deliveryGuy) {
-        log.debug("Deleting Delivery Guy.");
-        deliveryGuyService.delete(deliveryGuy);
-    }
-
-    @DeleteMapping(value = "/deliveryguys/{deliveryGuyId}")
-    public void deleteDeliveryGuyById(@PathVariable Long deliveryGuyId) {
-        log.debug("Deleting Delivery Guy by id.");
-        deliveryGuyService.deleteById(deliveryGuyId);
-    }
-
-    @GetMapping(value = "/deliveryguys/{deliveryGuyId}/orders")
-    public List<Order> getDeliveryGuyOrders(@PathVariable Long deliveryGuyId) {
-        return deliveryGuyService.getDeliveryGuyOrders(deliveryGuyId);
-    }
-
-    @GetMapping(value = "/deliveryguys/{deliveryGuyId}/orders/{orderId}")
-    public Order getDeliveryGuyOrderById(@PathVariable Long deliveryGuyId, @PathVariable Long orderId) {
-        return deliveryGuyService.getDeliveryGuyOrderById(deliveryGuyId, orderId);
+        return ResponseEntity.status(HttpStatus.OK).body(deliveryGuyService.update(deliveryGuy, deliveryGuyId));
     }
 
     @PutMapping(value = "/deliveryguys/{deliveryGuyId}/orders/{orderId}")
-    public Order modifyOrder(@PathVariable Long deliveryGuyId, @PathVariable Long orderId, @RequestBody OrderModificationVO modification) {
+    public ResponseEntity<Order> modifyOrder(@PathVariable Long deliveryGuyId, @PathVariable Long orderId, @RequestBody OrderModificationVO modification) {
         log.debug("Modifying Delivery Guy Order.");
-        return deliveryGuyService.modifyOrder(deliveryGuyId, orderId, modification);
+        return ResponseEntity.status(HttpStatus.OK).body(deliveryGuyService.modifyOrder(deliveryGuyId, orderId, modification));
+    }
+
+    @DeleteMapping(value = "/deliveryguys")
+    public ResponseEntity<?> deleteDeliveryGuy(@RequestBody DeliveryGuy deliveryGuy) {
+        log.debug("Deleting Delivery Guy.");
+        deliveryGuyService.delete(deliveryGuy);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping(value = "/deliveryguys/{deliveryGuyId}")
+    public ResponseEntity<?> deleteDeliveryGuyById(@PathVariable Long deliveryGuyId) {
+        log.debug("Deleting Delivery Guy by id.");
+        deliveryGuyService.deleteById(deliveryGuyId);
+        return ResponseEntity.noContent().build();
     }
 }
