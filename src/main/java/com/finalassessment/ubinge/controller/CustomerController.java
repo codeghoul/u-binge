920469c322b8137a2bffe601d28fe5c9b3476853
Jayesh <jayesh.patel.2197@gmail.com>
2019-08-26 11:34:46 +0530

    Modify CustomerController to return ResponseEntities.

--- src/main/java/com/finalassessment/ubinge/controller/CustomerController.java
+++ src/main/java/com/finalassessment/ubinge/controller/CustomerController.java
@@ -6,6 +6,8 @@ import com.finalassessment.ubinge.service.CustomerService;
 import com.finalassessment.ubinge.vo.OrderModificationVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,55 +23,58 @@ public class CustomerController {
     }
 
     @GetMapping(value = "/customers")
-    public List<Customer> getAllCustomers() {
+    public ResponseEntity<List<Customer>> getAllCustomers() {
         log.debug("Getting All Customers.");
-        return customerService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.findAll());
     }
 
     @GetMapping(value = "/customers/{customerId}")
-    public Customer getCustomer(@PathVariable Long customerId) {
+    public ResponseEntity<Customer> getCustomer(@PathVariable Long customerId) {
         log.debug("Getting Customers By Id.");
-        return customerService.findById(customerId);
-    }
-
-    @PostMapping(value = "/customers")
-    public Customer saveCustomer(@RequestBody Customer customer) {
-        log.debug("Saving Customer.");
-        return  customerService.save(customer);
-    }
-
-    @PutMapping(value = "/customers/{customerId}")
-    public Customer updateCustomer(@RequestBody Customer customer, @PathVariable Long customerId) {
-        log.debug("Updating Customer.");
-        return customerService.update(customer, customerId);
-    }
-
-    @DeleteMapping(value = "/customers")
-    public void deleteCustomer(@RequestBody Customer customer) {
-        log.debug("Deleting Customer.");
-        customerService.delete(customer);
-    }
-
-    @DeleteMapping(value = "/customers/{customerId}")
-    public void deleteCustomerById(@PathVariable Long customerId) {
-        log.debug("Deleting Customer By Id.");
-        customerService.deleteById(customerId);
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.findById(customerId));
     }
 
     @GetMapping(value = "/customers/{customerId}/orders")
-    public List<Order> getCustomerOrders(@PathVariable Long customerId) {
+    public ResponseEntity<List<Order>> getCustomerOrders(@PathVariable Long customerId) {
         log.debug("Getting Customer Orders.");
-        return customerService.getCustomerOrders(customerId);
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.getCustomerOrders(customerId));
     }
 
     @GetMapping(value = "/customer/{customerId}/orders/{orderId}")
-    public Order getCustomerOrderById(@PathVariable Long customerId, @PathVariable Long orderId){
-        return customerService.getCustomerOrderById(customerId, orderId);
+    public ResponseEntity<Order> getCustomerOrderById(@PathVariable Long customerId, @PathVariable Long orderId){
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.getCustomerOrderById(customerId, orderId));
+    }
+
+    @PostMapping(value = "/customers")
+    public ResponseEntity<Customer> saveCustomer(@RequestBody Customer customer) {
+        log.debug("Saving Customer.");
+        customerService.save(customer);
+        return ResponseEntity.status(HttpStatus.CREATED).body(customerService.save(customer));
+    }
+
+    @PutMapping(value = "/customers/{customerId}")
+    public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer, @PathVariable Long customerId) {
+        log.debug("Updating Customer.");
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.update(customer, customerId));
     }
 
     @PutMapping(value = "/customers/{customerId}/orders/{orderId}")
-    public Order modifyOrder(@PathVariable Long customerId, @PathVariable Long orderId, @RequestBody OrderModificationVO modification) {
+    public ResponseEntity<Order> modifyOrder(@PathVariable Long customerId, @PathVariable Long orderId, @RequestBody OrderModificationVO modification) {
         log.debug("Modifying Customer Order.");
-        return customerService.modifyOrder(customerId, orderId, modification);
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.modifyOrder(customerId, orderId, modification));
+    }
+
+    @DeleteMapping(value = "/customers")
+    public ResponseEntity<?> deleteCustomer(@RequestBody Customer customer) {
+        log.debug("Deleting Customer.");
+        customerService.delete(customer);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping(value = "/customers/{customerId}")
+    public ResponseEntity<?> deleteCustomerById(@PathVariable Long customerId) {
+        log.debug("Deleting Customer By Id.");
+        customerService.deleteById(customerId);
+        return ResponseEntity.noContent().build();
     }
 }
