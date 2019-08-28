4fb91b15e75141f58c085c68285c3f5252a27fba
Jayesh <jayesh.patel.2197@gmail.com>
2019-08-28 09:12:22 +0530

    ADMIN role has access to admin paths.

--- src/main/java/com/finalassessment/ubinge/config/SecurityConfiguration.java
+++ src/main/java/com/finalassessment/ubinge/config/SecurityConfiguration.java
@@ -47,7 +47,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/securityNone").permitAll()
+                .antMatchers("/admin/**").access("hasAuthority('ADMIN')")
                 .anyRequest().authenticated()
                 .and().csrf().disable()
                 .httpBasic()
